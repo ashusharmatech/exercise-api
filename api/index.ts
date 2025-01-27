dc85107851a709b96aaed5aba7a2d781263a83d5
Ashutosh Sharma <email2ashusharma@gmail.com>
2025-01-27 23:29:15 +0530

    first commit

---
+++ api/index.ts
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const { sql } = require('@vercel/postgres');
+
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// Create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'home.htm'));
+});
+
+app.get('/about', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'about.htm'));
+});
+
+app.get('/uploadUser', function (req, res) {
+	res.sendFile(path.join(__dirname, '..', 'components', 'user_upload_form.htm'));
+});
+
+
+app.listen(3000, () => console.log('Server ready on port 3000.'));
+
+module.exports = app;
